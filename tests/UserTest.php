1641bbbb401df5b7d1269fda67fe043a1f4f24d8
Jalid <chaerj91@gmail.com>
2022-12-11 15:46:03 -0400

    Added test getUser

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -14,5 +14,15 @@ class UserTest extends TestCase
         $this->assertEquals($user->get_phone(), '4446556');
         $this->assertEquals($user->get_address(), 'street 1');
     }
+
+
+    public function testgetUser(){
+        $user = new User('Joe','joe@gmail.com','1234','4446556','street 1'); 
+        $result = $user->getUser();
+        $this->assertEquals($result['name'], 'Joe');
+        $this->assertEquals($result['email'], 'joe@gmail.com');
+        $this->assertEquals($result['phone'], '4446556');
+        $this->assertEquals($result['address'], 'street 1');
+    }
     
 }
